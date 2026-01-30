4d20b3dd5ca5089b57fb9a3d69143862c3dbed8a
PatrickChodowski <chodowski.patrick@gmail.com>
2026-01-30 09:16:48 +0100

    Remove camera changed event

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod utils;
 pub mod prelude {
     pub use crate::assets::{PGCoreAssetsPlugin, ConfigHandles, AssetHandles};
     pub use crate::camera::{PGCoreCameraPlugin, InputState, MainCamera, 
-        fly_cam_controller, FlyCamController, is_camera_dev, is_camera_player, CameraModeChanged};
+        fly_cam_controller, FlyCamController, is_camera_dev, is_camera_player};
     pub use crate::game_state::{PGCoreStatesPlugin, GameState, GameStatePlay, GameStateInit, GameStateTransition};
     pub use crate::player::Player;
     pub use crate::utils::{AABB, rotate_point_2d};
